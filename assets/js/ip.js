b6fec5587272eca6c6bb01a1554630c0e93dbdcf
Catalyst-Craft <92801262+Catalyst-Craft@users.noreply.github.com>
2022-03-07 11:41:28 -0600

    changed data collected

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -3,10 +3,10 @@ fetch('https://ipapi.co/json/').then(function(response) {
   return response.json();
 }).then(function(data) {
   document.getElementById("ipaddress").value = (data.ip);
+  document.getElementById("iptime").value = (data.timezone);
   document.getElementById("ipcity").value = (data.city);
   document.getElementById("ipregion").value = (data.region);
   document.getElementById("ipcounty").value = (data.country_name);
-  document.getElementById("ipcontinent").value = (data.continent_name);
   document.getElementById("iplatitude").value = (data.latitude);
   document.getElementById("iplongitude").value = (data.longitude);
 }).catch(function() {
